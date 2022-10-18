bdad3b987293349c4d688786af929922e3557234
MartinYordanov12 <104901683+MartinYordanov12@users.noreply.github.com>
2022-10-18 16:23:17 +0300

    Task3, implement reflection lib

---
+++ store-runner/src/main/java/com/coherentsolutions/store/service/CatalogReflectionLibLoader.java
@@ -0,0 +1,26 @@
+package com.coherentsolutions.store.service;
+
+import com.coherentsolutions.store.domain.Category;
+import exceptions.CategoryException;
+import org.reflections.Reflections;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CatalogReflectionLibLoader {
+    public List<Category> loadClassesFromPackage(final String categoriesPackage) {
+        Reflections reflections = new Reflections(categoriesPackage);
+        Set<Class<? extends Category>> subTypes = reflections.getSubTypesOf(Category.class);
+
+        return subTypes.stream()
+                .map(type -> {
+                    try {
+                        return type.getConstructor().newInstance();
+                    } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+                        throw new CategoryException(String.format("New category %s can not be created!", type.getName()));
+                    }
+                }).collect(Collectors.toList());
+    }
+}
+
